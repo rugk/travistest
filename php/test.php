6823b79bd775f0226227e14c5ec03131a94aff44
rugk <rugk@posteo.de>
2016-09-18 15:47:26 +0200

    Add test scripts

---
+++ php/test.php
@@ -0,0 +1,5 @@
+#!/usr/bin/env php
+<?php 
+# show constant value
+echo CURLOPT_PINNEDPUBLICKEY;
+?>
---
+++ shell/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ex
+
+# Install recent curl version
+# TODO
+
+echo "Show me value: $( php -r 'echo CURLOPT_PINNEDPUBLICKEY;' )"
