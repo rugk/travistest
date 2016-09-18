896fa80364981ce3216ccaf8c4076e9f414a33ab
rugk <rugk@posteo.de>
2016-09-18 19:57:56 +0200

    Remove old code

--- shell/test.sh
+++ shell/test.sh
@@ -32,22 +32,3 @@ sudo make install
 
 # show curl version
 curl --version
-locate curl
-
-# get and trust keys
-# source: https://secure.php.net/downloads.php#gpg-7.0
-# gpg --recv-keys "1A4E 8B72 77C4 2E53 DBA9 C7B9 BCAA 30EA 9C0D 5763"
-# echo "1A4E8B7277C42E53DBA9C7B9BCAA30EA9C0D576:6:"|gpg --import-ownertrust
-#
-# # download PHP
-# wget $PHP_MIRRROR/php-$PHP_VERSION.tar.bz2$PHP_MIRRROR_APPEND
-# wget $PHP_MIRRROR/php-$PHP_VERSION.tar.bz2.asc$PHP_MIRRROR_APPEND
-#
-# gpg --verify php-$PHP_VERSION.tar.bz2.asc
-#
-# bzip2 -d php-$PHP_VERSION.tar.bz2
-# tar -xf php-$PHP_VERSION.tar
-#
-# # build PHP with curl
-# cd php-$PHP_VERSION
-# ./configure --with-curl
